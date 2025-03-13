57bd8fdeb81634a33ee3cf4db2a760ab4cb6a664
oleg <op278381@gmail.com>
2025-03-13 20:15:51 +0200

    доработка mobile

--- slick/script.js
+++ slick/script.js
@@ -47,3 +47,56 @@ $(document).ready(function () {
     ],
   });
 });
+
+// Slider-basket
+
+$(document).ready(function () {
+  $(".slider-basket").slick({
+    arrows: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1191,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 918,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 380,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+        },
+      },
+    ],
+  });
+});
+
